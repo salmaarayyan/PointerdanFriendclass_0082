82a0dc2a20aade6a9b6662dfadb94aac930bef0c
salmaarayyan <salmaarayyan@gmail.com>
2024-05-06 15:19:49 +0700

    membuat int main

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -17,14 +17,13 @@ class siswa
     private:
         int id;
     public:
-        void setId(int PId);
+        void setId(int pId);
         void displayAll(orang &a);
 };
 
 void siswa::displayAll(orang &a)
 {
-    cout << id << endl
-         << a.nama;
+    cout << id << endl << a.nama;
 }
 
 void orang::setNama(string pNama)
@@ -35,4 +34,14 @@ void orang::setNama(string pNama)
 void siswa::setId(int pId)
 {
     id = pId;
+}
+
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
 }
